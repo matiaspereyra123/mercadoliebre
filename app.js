4f3d61d6e6f50bf250e7aa7d2c598e48676f6726
matiaspereyra123 <raulmatiaspereyra@gmail.com>
2022-04-20 20:16:57 -0300

    process start

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const publicPath=path.resolve(__dirname,'./public');
 
 app.use(express.static(publicPath));
 
-app.listen(3030,()=>{
+app.listen(process.env.PORT ||3030,()=>{
     console.log("Servidor corriendo en el puerto 3030");
 });
 
